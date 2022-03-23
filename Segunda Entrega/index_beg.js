4494aca43b74106146040005be4d6edbeb520e53
Daniel Forero <danielforerocorredor.99@gmail.com>
2022-03-23 10:02:47 -0500

    Add files via upload

--- Segunda Entrega/index_beg.js
+++ Segunda Entrega/index_beg.js
@@ -1,9 +1,9 @@
 function logIn(){
-    document.write("HOLA");
+    //document.write("HOLA");
     window.location = "logIn.html";
 }
 
 function signUp(){
-    document.write("HOLA");
+    //document.write("HOLA");
     window.location = "register.html";
 }
