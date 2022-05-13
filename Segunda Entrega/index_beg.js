3174a9c4cbdb24d3749736f2377ad0a2ace17c73
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2022-05-13 08:23:49 -0500

    Delete Segunda Entrega directory

--- Segunda Entrega/index_beg.js
+++
@@ -1,9 +0,0 @@
-function logIn(){
-    //document.write("HOLA");
-    window.location = "logIn.html";
-}
-
-function signUp(){
-    //document.write("HOLA");
-    window.location = "register.html";
-}
